01dfb03474d96aef46dd391785a01a8f5817bde3
Eva Wong <suddenlyeva@gmail.com>
2022-03-30 20:16:06 -0700

    hello world and the basics of the basics

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+import game from '@/game'
+
+document.onkeydown = e => {
+  if (e.code != 'Enter') return
+  alert(game.next())
+}
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
